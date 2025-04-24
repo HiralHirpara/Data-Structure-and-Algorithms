5d9e56cf04542e6cd79d17e3ab7e67331b587a1b
HiralHirpara <hiralhirpara001@gmail.com>
2025-04-24 11:46:41 -0700

    added iterative dfs approach

--- src/Graph/DepthFirstSearch.java
+++ src/Graph/DepthFirstSearch.java
@@ -1,26 +1,44 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class DepthFirstSearch {
 
-    public static  void dfs(ArrayList<Edge>[] graph, boolean vis[], int curr){
-        if(vis[curr]){
+    public static void dfs1(List<Edge>[] graph, boolean visited[], int curr){
+        if(visited[curr]){
             return;
         }
-        vis[curr] = true;
+        visited[curr] = true; // Each node is visited once : O(V)
         System.out.println(curr);
-        for(int i = 0;i<graph[curr].size();i++){
+        for(int i = 0;i<graph[curr].size();i++){  // look at each edge once through the adjacency list : O(E)
             Edge e = graph[curr].get(i);
-            if(!vis[e.getDestination()]){
-                dfs(graph, vis, e.getDestination());
+            if(!visited[e.getDestination()]){
+                dfs1(graph, visited, e.getDestination());
+            }
+        }
+    }
+
+    public static void dfs2(List<Edge>[] graph, boolean visited[], Stack<Integer> stack){
+        while(!stack.isEmpty()) {
+            System.out.println(stack);
+            int curr = stack.pop();
+            if(!visited[curr]) {
+                visited[curr] = true; // Each node is visited once : O(V)
+                for (int i = 0; i < graph[curr].size(); i++) {  // look at each edge once through the adjacency list : O(E)
+                    Edge e = graph[curr].get(i);
+                    if (!visited[e.getDestination()]) {
+                        stack.push(e.getDestination());
+                    }
+                }
             }
         }
     }
     public static void main(String[] args) {
         // Array of Arraylist for Graph AdjacencyList implementation
         int V = 5;
-        ArrayList<Edge>[] graph = new ArrayList[V];
+        List<Edge>[] graph = new ArrayList[V];
 
         for(int i = 0;i< graph.length;i++){
             graph[i] = new ArrayList<>();
@@ -41,6 +59,15 @@ public class DepthFirstSearch {
 
         graph[4].add(new Edge(4,2));
 
-        dfs(graph, new boolean[V], 0);
+        System.out.println("DFS Technique 1: Recursive");
+        dfs1(graph, new boolean[V], 0);  // More efficient technique
+
+        System.out.println("\nDFS Technique 2: Iterative");
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        dfs2(graph, new boolean[V], stack);
+
+        // Time Complexity : O(V + E)
+        // Space Complexity : O(V)  : Visited array
     }
 }
