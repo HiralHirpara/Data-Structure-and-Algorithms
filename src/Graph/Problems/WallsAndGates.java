37bfd0d57a8d91bf7eaf31d313f750c9319e8fc1
HiralHirpara <hiralhirpara001@gmail.com>
2025-04-25 17:01:14 -0700

    added one more approach in Walls And Gates Graph problem

--- src/Graph/Problems/WallsAndGates.java
+++ src/Graph/Problems/WallsAndGates.java
@@ -8,23 +8,21 @@ public class WallsAndGates {
     private final int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
     private final int INF = 2147483647;
     private boolean[][] visit;
-    private int ROWS;
-    private int COLS;
-//    visit = new boolean[ROWS][COLS]; // only for DFS Approach
-    public void wallsAndGates(int[][] grid) {
-        ROWS = grid.length;
-        COLS = grid[0].length;
+    private static int ROWS;
+    private static int COLS;
+    private final int GATE = 0;
 
+    public void wallsAndGatesDFS(int[][] grid) {
+        visit = new boolean[ROWS][COLS]; // only for DFS Approach
          for(int i =0;i<ROWS;i++){
              for(int j = 0;j<COLS;j++){
                  if(grid[i][j] == INF){
-//                     grid[i][j] = dfs(grid, i, j); // DFS + Backtracking
-                     grid[i][j] = bfs(grid, i, j); // BFS
+                     grid[i][j] = dfs(grid, i, j); // DFS + Backtracking
                  }
              }
-
          }
     }
+
     private int dfs(int[][] grid, int r, int c){
         if(r < 0 || r >= ROWS || c < 0 || c >= COLS ||
         grid[r][c] == -1 || visit[r][c]){
@@ -45,6 +43,18 @@ public class WallsAndGates {
         return res;
     }
 
+    public void wallsAndGatesBFS(int[][] grid) {
+        for(int i =0;i<ROWS;i++){
+            for(int j = 0;j<COLS;j++){
+                if(grid[i][j] == INF){
+                    grid[i][j] = bfs(grid, i, j); // BFS via INF
+                }
+                if(grid[i][j] == GATE){
+                    grid[i][j] = bfs(grid, i, j); // BFS via INF
+                }
+            }
+        }
+    }
     private int bfs(int[][] grid, int r, int c){
         Queue<int[]> q = new LinkedList<>();
         visit = new boolean[ROWS][COLS];
@@ -74,17 +84,66 @@ public class WallsAndGates {
         return INF;
     }
 
+    public void wallsAndGatesBFSviaGATES(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        for(int i =0;i<ROWS;i++){
+            for(int j = 0;j<COLS;j++){
+                if(grid[i][j] == GATE){
+                   q.add(new int[] {i, j}); // BFS via GATES
+                }
+            }
+        }
+        if(q.size() == 0){
+             return;
+        }
+        while (!q.isEmpty()){
+            int[] node = q.remove();
+            int row = node[0];
+            int col = node[1];
+            for(int[] dir : directions){
+                int nr = row + dir[0];
+                int nc = col + dir[1];
+                if (nr >= ROWS || nc >= COLS || nr < 0 ||
+                        nc < 0 || grid[nr][nc] != Integer.MAX_VALUE) {
+                    continue;
+                }
+                q.add(new int[] { nr, nc });
+
+                grid[nr][nc] = grid[row][col] + 1;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[][] grid = {{2147483647,-1,0,2147483647},
                         {2147483647,2147483647,2147483647,-1},
                         {2147483647,-1,2147483647,-1},
                         {0,-1,2147483647,2147483647}};
 
-
+        ROWS = grid.length;
+        COLS = grid[0].length;
 
         WallsAndGates wallsAndGates = new WallsAndGates();
-        wallsAndGates.wallsAndGates(grid);
-        System.out.println("Result is :");
+        wallsAndGates.wallsAndGatesDFS(grid);
+        System.out.println("DFS Result is :");
+        for(int[] g : grid){
+            System.out.println(Arrays.toString(g));
+        }
+        grid = new int[][]{{0, -1}, {1, 2}};
+
+        ROWS = grid.length;
+        COLS = grid[0].length;
+        System.out.println("\nBFS via INF Result is :");
+        wallsAndGates.wallsAndGatesDFS(grid);
+        for(int[] g : grid){
+            System.out.println(Arrays.toString(g));
+        }
+        grid = new int[][]{{0, -1}, {1, 2}};
+
+        ROWS = grid.length;
+        COLS = grid[0].length;
+        System.out.println("\nBFS via GATES Result is :");
+        wallsAndGates.wallsAndGatesBFSviaGATES(grid);
         for(int[] g : grid){
             System.out.println(Arrays.toString(g));
         }
