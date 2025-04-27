9bf2fdd11ce6c41a67d92c7a65bf756a25a5ccd6
HiralHirpara <hiralhirpara001@gmail.com>
2025-04-27 16:56:12 -0700

    added Number of Connected Components in Undirected Graph problem

---
+++ src/Graph/Problems/NumberOfConnectedComponentsInUG.java
@@ -0,0 +1,83 @@
+package Graph.Problems;
+
+import java.util.*;
+
+public class NumberOfConnectedComponentsInUG {
+
+    private int countComponents(int n, int[][] edges) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i = 0;i<n;i++){
+            graph.put(i, new ArrayList<>());
+        }
+        for(int i[] : edges){
+            graph.get(i[0]).add(i[1]);
+            graph.get(i[1]).add(i[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        int result = 0;
+
+        for(int i = 0;i<n;i++){
+            if(!visited[i]){
+                dfs(graph, visited, i);
+                result++;
+            }
+        }
+        return result;
+    }
+    private static void dfs(Map<Integer, List<Integer>> graph, boolean[] visited, int src){
+        visited[src] = true;
+        for(int i: graph.get(src)){
+            if(!visited[i]){
+                dfs(graph, visited, i);
+            }
+        }
+    }
+    private int countComponentsBFS(int n, int[][] edges) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i = 0;i<n;i++){
+            graph.put(i, new ArrayList<>());
+        }
+        for(int i[] : edges){
+            graph.get(i[0]).add(i[1]);
+            graph.get(i[1]).add(i[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        int result = 0;
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0;i<n;i++){
+            if(!visited[i]){
+                q.add(i);
+                visited[i] = true;
+               while (!q.isEmpty()){
+                   int curr = q.remove();
+                   for(int edge : graph.get(curr)){
+                       if(!visited[edge]){
+                           visited[edge] = true;
+                           q.add(edge);
+                       }
+                   }
+               }
+                result++;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+
+        NumberOfConnectedComponentsInUG ng = new NumberOfConnectedComponentsInUG();
+
+        int n = 5;
+        int[][] edges = {{0,1},{1,2}, {3,4}};
+
+        System.out.println("\n:::::::Number of Connected Components in an Undirected Graph:::::::\n");
+
+        // T(n) : O(V + E)
+        System.out.println("Result of DSF approach: " + ng.countComponents(n, edges));
+
+        edges = new int[][] {{0, 1}, {0,2}, {1, 3}, {3, 4}};
+         System.out.println("Result of BSF approach: " + ng.countComponentsBFS(n, edges));
+    }
+}
